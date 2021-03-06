f0e8b957d192a47a851637781ccff48d69f47f71
unknown <ugiut@student.kit.edu>
2021-03-06 20:41:24 +0100

    Validations v2 for EU hub

---
+++ codebase/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 13:46:59 2021
+
+@author: Jannik
+"""
---
+++ main.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 19:04:35 2021
+
+@author: Jannik
+"""
+
+import re
+import os
+import json
+import glob
+from github import Github
+import urllib.request
+import sys
+import shutil
+
+from codebase.test_formatting import forecast_check
+from codebase.test_formatting import forecast_check, validate_forecast_file, print_output_errors
+from codebase.validation_functions.metadata import check_for_metadata, get_metadata_model, output_duplicate_models
+
+# Pattern that matches a forecast file added to the data-processed folder.
+# Test this regex usiing this link: https://regex101.com/r/wmajJA/1
+pat = re.compile(r"^data-processed/(.+)/\d\d\d\d-\d\d-\d\d-\1\.csv$")
+pat_meta = re.compile(r"^data-processed/(.+)/metadata-\1\.txt$")
+pat_other = re.compile(r"^data-processed/(.+)\.csv$")
+
+local = os.environ.get('CI') != 'true'
+# local = True
+if local:
+    token = None
+    print("Running on LOCAL mode!!")
+else:
+    print("Added token")
+    token  = os.environ.get('GH_TOKEN')
+    print(f"Token length: {len(token)}")
+
+if token is None:
+    g = Github()
+else:
+    g = Github(token)
+repo_name = os.environ.get('GITHUB_REPOSITORY')
+if repo_name is None:
+    repo_name = 'epiforecasts/covid19-forecast-hub-europe'
+repo = g.get_repo(repo_name)
+
+print(f"Github repository: {repo_name}")
+print(f"Github event name: {os.environ.get('GITHUB_EVENT_NAME')}")
+
+if not local:
+    event = json.load(open(os.environ.get('GITHUB_EVENT_PATH')))
+else:
+    event = json.load(open("test/test_event.json"))
+    
+pr = None
+comment = ''
+files_changed = []
+
+if os.environ.get('GITHUB_EVENT_NAME') == 'pull_request_target' or local:
+    # Fetch the  PR number from the event json
+    pr_num = event['pull_request']['number']
+    print(f"PR number: {pr_num}")
+
+    # Use the Github API to fetch the Pullrequest Object. Refer to details here: https://pygithub.readthedocs.io/en/latest/github_objects/PullRequest.html 
+    # pr is the Pullrequest object
+    pr = repo.get_pull(pr_num)
+
+    # fetch all files changed in this PR and add it to the files_changed list.
+    files_changed +=[f for f in pr.get_files()]
+
+forecasts = [file for file in files_changed if pat.match(file.filename) is not None]
+forecasts_err = [file for file in files_changed if pat_other.match(file.filename) is not None]
+metadatas = [file for file in files_changed if pat_meta.match(file.filename) is not None]
+other_files = [file for file in files_changed if (pat.match(file.filename) is None and pat_meta.match(file.filename) is None)]
+
+if os.environ.get('GITHUB_EVENT_NAME') == 'pull_request_target':
+    # IF there are other fiels changed in the PR 
+    #TODO: If there are other files changed as well as forecast files added, then add a comment saying so. 
+    if len(other_files) > 0 and len(forecasts) >0:
+        print(f"PR has other files changed too.")
+        if pr is not None:
+            pr.add_to_labels('other-files-updated')
+    
+    if len(metadatas) > 0:
+        print(f"PR has metata files changed.")
+        if pr is not None:
+            pr.add_to_labels('metadata-change')
+
+    if len(forecasts) > 0:
+        if pr is not None:
+            pr.add_to_labels('data-submission')
+        
+    deleted_forecasts = False
+    changed_forecasts = False
+    
+    # `f` is ab object of type: https://pygithub.readthedocs.io/en/latest/github_objects/File.html 
+    # `forecasts` is a list of `File`s that are changed in the PR.
+    for f in forecasts:
+        # check if file is remove
+        if f.status == "removed":
+            deleted_forecasts = True
+        
+        # if file status is not "added" it is probably "renamed" or "changed"
+        elif f.status != "added":
+            changed_forecasts = True
+
+    if deleted_forecasts:
+        pr.add_to_labels('forecast-deleted')
+        comment += "\n Your submission seem to have deleted some forecasts. Could you provide a reason for the updation/deletion? Thank you!\n\n"
+        
+    if changed_forecasts:
+        pr.add_to_labels('forecast-updated')
+        comment += "\n Your submission seem to have updated/renamed some forecasts. Could you provide a reason for the updation/deletion? Thank you!\n\n"
+    
+# Download all forecasts
+# create a forecasts directory
+os.makedirs('forecasts', exist_ok=True)
+
+# Download all forecasts changed in the PR into the forecasts folder
+for f in forecasts:
+    urllib.request.urlretrieve(f.raw_url, f"forecasts/{f.filename.split('/')[-1]}")
+
+# Download all metadat files changed in the PR into the forecasts folder
+for f in metadatas:
+    urllib.request.urlretrieve(f.raw_url, f"forecasts/{f.filename.split('/')[-1]}")
+    
+# Run validations on each of these files
+errors = {}
+for file in glob.glob("forecasts/*.csv"):
+    error_file = forecast_check(file)
+    if len(error_file) >0:
+        errors[os.path.basename(file)] = error_file
+
+FILEPATH_META = "forecasts/"
+is_meta_error, meta_err_output = check_for_metadata(filepath=FILEPATH_META)
+
+# look for .csv files that dont match pat regex
+for file in other_files:
+    if file.filename[:14] == "data-processed" and ".csv" in file.filename:
+        
+        print(file.filename)
+        err_message = "File does not match forecast file naming convention: <date>-<team>-<model>.csv"
+        errors[file.filename] = [err_message]
+
+if len(errors) > 0:
+    comment+="\n\n Your submission has some validation errors. Please check the logs of the build under the \"Checks\" tab to get more details about the error. "
+    print_output_errors(errors, prefix='data')
+
+if is_meta_error:
+    comment+="\n\n Your submission has some metadata validation errors. Please check the logs of the build under the \"Checks\" tab to get more details about the error. "
+    print_output_errors(meta_err_output, prefix="metadata")
+
+# add the consolidated comment to the PR
+if comment!='' and not local:
+    pr.create_issue_comment(comment)
+
+shutil.rmtree("forecasts")
+
+if is_meta_error or len(errors)>0:
+    sys.exit("\n ERRORS FOUND EXITING BUILD...")
