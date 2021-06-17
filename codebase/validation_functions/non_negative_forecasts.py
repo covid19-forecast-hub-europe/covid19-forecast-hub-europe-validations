f40dd62887ee5a226ce4640bca108326dda3f84b
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2021-06-17 17:59:24 +0100

    Update non_negative_forecasts.py

--- codebase/validation_functions/non_negative_forecasts.py
+++ codebase/validation_functions/non_negative_forecasts.py
@@ -26,12 +26,12 @@ def non_negative_values(file):
     
     
     if negative.any():
-        result.append(f"Warning > negative value in forecast")
+        result.append(f": negative value in forecast")
             
     non_integer = df["value"] != type(int)
     
     if non_integer.any():
-        result.append(f"Warning > non-integer value in forecast")
+        result.append(f" : non-integer value in forecast")
 
     if len(result) > 0:
         result = 'Warnings' + ''.join(result)
